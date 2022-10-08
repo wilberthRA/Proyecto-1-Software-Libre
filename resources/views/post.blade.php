f8b93e1bb2ba6a3dd85f50153198445264c23bf3
wilberthRA <61328696+wilberthRA@users.noreply.github.com>
2022-10-08 12:29:41 -0600

    Make-a-Route-and-Link-to-it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit voluptas reiciendis eveniet odio quam, perferendis in blanditiis labore minima saepe hic praesentium delectus? A nesciunt possimus quis, fugiat nisi expedita.
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit voluptas reiciendis eveniet odio quam, perferendis in blanditiis labore minima saepe hic praesentium delectus? A nesciunt possimus quis, fugiat nisi expedita.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit voluptas reiciendis eveniet odio quam, perferendis in blanditiis labore minima saepe hic praesentium delectus? A nesciunt possimus quis, fugiat nisi expedita.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit voluptas reiciendis eveniet odio quam, perferendis in blanditiis labore minima saepe hic praesentium delectus? A nesciunt possimus quis, fugiat nisi expedita.
+        </p>
+    </article>    
+</body>
+</html>
