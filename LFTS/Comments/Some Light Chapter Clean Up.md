713d2417e35cd46f6e8cfcb247b87b2476c7a34e
wilberthRA <61328696+wilberthRA@users.noreply.github.com>
2022-10-22 19:13:39 -0600

    Some Light Chapter Clean Up

---
+++ LFTS/Comments/Some Light Chapter Clean Up.md
@@ -0,0 +1,6 @@
+
+# Some Light Chapter Clean Up.
+
+Algunos cambios para hacer que el codigo se vea de una manera mas limpia.
+
+[ver cambios en el codigo]()
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,27 +52,7 @@
                     </div>
                 </div>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    @auth
-                    <x-panel>
-                        <form action="/posts/{{$post->slug}}/comments" method="post" class="border border-gray-200 p-6 rounded-xl">
-                            @csrf
-                            <header class="flex items-center">
-                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
-                                <h2 class="ml-4">Want to participate</h2>
-                            </header>
-                            <div class="mt-6">
-                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="10" placeholder="Quick, thing of something to say !"></textarea>
-                            </div>
-                            <div class="flex justify-end">
-                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                            </div>
-                        </form>
-                    </x-panel>
-                    @else
-                    <p class="font-semibold">
-                        <a href="/register" class="hover:underline>">Register</a> or <a href="/login" class="hover:underline">log in</a> to leave a comment.
-                    </p>
-                    @endauth
+                @include ('posts._add-comment-form')
 
                 @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
