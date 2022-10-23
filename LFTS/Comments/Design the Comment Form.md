1bd2863b8fadd8a1b284b853293a954a88b415a8
wilberthRA <61328696+wilberthRA@users.noreply.github.com>
2022-10-22 18:06:22 -0600

    Design the Comment Form

---
+++ LFTS/Comments/Design the Comment Form.md
@@ -0,0 +1,6 @@
+
+# Design the Comment Form. 
+
+En este episodio se creo el form el cual permite la creacrion de un comentario en alguno de los posts.
+
+[ver cambios en el codigo]()
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,6 +1,7 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+<x-panel class="bg-gray-100">
+<article class="flex bg-gray-100 border space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
@@ -20,3 +21,4 @@
         </p>
     </div>
 </article>
+</x-panel>
