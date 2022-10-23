066f0ddabe549d65a775b6e30565b12f8c0254cd
wilberthRA <61328696+wilberthRA@users.noreply.github.com>
2022-10-23 01:15:44 -0600

    episodios 58-60

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,29 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+    $mailchimp->setConfig([
+        'apikey'=> config('services.mailchimp.key'),
+        'server'=> 'us21'
+    ]);
+    $response= $mailchimp->lists->getAllLists();
+    ddd($response);
+});
+
+
 Route::get('/', [PostsController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostsController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
